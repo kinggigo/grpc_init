a55ecbd17c63aebf755094034d5b3cd5e2cba8d5
dongha <donghaa90@gmail.com>
2022-01-20 13:27:33 +0900

    generate.sh update - go for server, dart for client

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #protoc --go_out=. --go_opt=paths=source_relative greet/greetpb/greet.proto
 
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative greet/greetpb/greet.proto
+# go - server, client, dart - client
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative  --dart_out=grpc:greet/greetpb/dart  greet/greetpb/greet.proto
